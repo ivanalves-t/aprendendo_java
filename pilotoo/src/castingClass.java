abd970230b4411fbc4403a89c4d0f3ab9e928d5b
Ivan <ivanilsonat@outlook.com>
2024-01-23 19:57:04 -0300

    primeiroCommitJava

---
+++ pilotoo/src/castingClass.java
@@ -0,0 +1,11 @@
+public class castingClass {
+    public static void main(String[] args) {
+        double a;
+        int b;
+
+        a = 5.2;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
